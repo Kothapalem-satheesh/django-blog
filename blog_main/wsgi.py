de1ff874b6583d64c531b8e6354bc3d4c8cc4929
KOTHAPALEM SATHEESH <Satheeshyadav85@gmail.com>
2026-02-09 15:24:55 +0530

    updating the files

--- blog_main/wsgi.py
+++ blog_main/wsgi.py
@@ -1,3 +1,21 @@
+# """
+# WSGI config for blog_main project.
+
+# It exposes the WSGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+# """
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_main.settings')
+
+# application = get_wsgi_application()
+
+
 """
 WSGI config for blog_main project.
 
@@ -8,9 +26,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add your project directory to the sys.path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_main.settings')
 
 application = get_wsgi_application()
+
+# Vercel requires this variable to be named 'app'
+app = application
